23686f0f116b8e3b6c4cb075e7f2be5d52cf2ce1
Brayan Ceron <brayan0np@gmail.com>
2023-02-19 14:46:36 -0500

    app_cargarArchivos_sin_autenticacion

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +15,7 @@ class _HomePageState extends State<HomePage> {
 
   Map argumentosRecividos= new Map();
 
+
   @override
   Widget build(BuildContext context) {
     argumentosRecividos = (ModalRoute.of(context)?.settings.arguments) as Map;
@@ -18,7 +23,69 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       appBar: AppBar(title: Text("Page Home"),),
+      body: ListView(
+        children: [
+          Text("ok"),
+          MaterialButton(
+            child: Text("Seleccion Archivo"),
+            color: Colors.blueAccent,
+            onPressed: selecionar_archivo
+            ),
+          //Image.network('http://192.168.1.57:8000/media/Screenshot_20230218-013239_D8S4Wda.png'),
+        ],
+      ),
       
     );
   }
+
+   selecionar_archivo() async {
+        FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true);
+        var URL = Uri.parse('http://192.168.1.57:8000/subirArchivotest/');
+
+        if(result != null) {
+          //PlatformFile file = result.files.first;
+          List<File> files = result.paths.map((path) => File(path.toString())).toList();
+          
+          print("--------------");
+          for (File file in files){
+              /*print(file.name);
+              print(file.bytes);
+              print(file.size);
+              print(file.extension);*/
+              print(file.path); 
+
+    
+              
+              var request = http.MultipartRequest('POST', URL);
+              request.files.add(await http.MultipartFile.fromPath("Myarchivo", file.path.toString()));
+              var res = await request.send()
+              .then((response){                
+
+                print(response.toString());
+                if (response.statusCode == 200) print('Uploaded!');
+                /*
+                http.Response.fromStream(response).then((onValue){
+                  print(response.statusCode);                             
+                });
+                */
+                
+              });
+          }
+          print("--------------");
+                  
+          //print(res.body);
+
+          //print(res.reasonPharse);          
+          //return res.reasonPhrase;
+          return "0";
+
+        } else {
+          // Usuario cancela carga
+        }
+  }
+
+
+
+
+
 }
